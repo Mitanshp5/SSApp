e6abde4f56251ede13d2d977bd4de9d1ce88b138
Mitansh Prajapati <mitanshp2112@gmail.com>
2025-12-16 22:09:36 +0530

    PLC Connection eding Gracefully

--- SSApp.UI/App.xaml.cs
+++ SSApp.UI/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Runtime.InteropServices;
+using SSApp.Services.Logging;
 
 namespace SSApp.UI
 {
@@ -9,6 +11,22 @@ namespace SSApp.UI
     /// </summary>
     public partial class App : Application
     {
-    }
+        [DllImport("SSApp.Native.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern void DisconnectPlc();
 
+        protected override void OnExit(ExitEventArgs e)
+        {
+            try
+            {
+                Logger.LogInformation("Application exiting. Disconnecting PLC...");
+                DisconnectPlc();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Error during application shutdown", ex);
+            }
+
+            base.OnExit(e);
+        }
+    }
 }
